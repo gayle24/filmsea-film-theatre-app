312fbc31a44a7f4f0880f48d0561bebd40715d96
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-10-03 22:57:45 +0300

    creating domain image for readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# FilmSea Application
+<strong><u>Movies that Make Waves</u></strong>
+
+FilmSea is an app that supports new filmmakers by making their films available to watch in partner theatres as well as making them known to the general public
+
+
+
+<img src="domain.svg" alt="Domain Image">
--- server/models.py
+++ server/models.py
@@ -8,12 +8,23 @@ class FilmMaker(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    theatres = db.relationship('Theatres', secondary='films', back_populates='filmmakers')
+    theatres = db.relationship('Theatre', secondary='films', back_populates='filmmakers')
 
 class Theatre(db.Model, SerializerMixin):
     __tablename__ = 'theatres'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    capacity = db.Column(db.Integer)
-    ticket_price = db.Column
+    ticket_price = db.Column(db.Integer)
+
+    filmmakers = db.relationship('Filmmaker', secondary='Films', back_populates='theatres')
+
+class Films(db.Model, SerializerMixin):
+    __tablename__ = 'films'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    filmmaker_id = db.Column(db.Integer, ForeignKey='filmmakers.id')
+    theatre_id = db.Column(db.Integer, ForeignKey='theatres.id')
+    duration = db.Column(db.Integer)
+    screening_time = db.Column(db.DateTime)
