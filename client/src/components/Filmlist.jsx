2754ee59d04a8bbe225bf0f8fb04c334e3b6621e
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-10-09 08:13:38 +0300

    filmlist styling

--- client/src/components/Filmlist.jsx
+++ client/src/components/Filmlist.jsx
@@ -2,6 +2,20 @@ import { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom"; // Import Link to create links for booking tickets
 import '../App.css';
 
+const backgroundStyle = {
+  backgroundImage: `url('https://cdn.gencraft.com/prod/user/8b02d5ef-3e8a-4c1e-a4ec-0d1bfc053fc3/9224bb16-9593-4328-b371-d280d2ded984/images/image1_0.jpg?Expires=1696914451&Signature=HUogeiKA0jnkOIbD54ibqrzO68oXDVenHOxqnMtGrRvXsHpExSu7V3V-7xqn1MvLAPi0LfPZ3Z2vXNZuKtTrvZRCmBnjhwYXEw2qQ3v6J6okckclrdqseMXEBaJTgovLJ0hzLv9sOVr9WuyIoSScgvD2J~2oFUP5W1I0~SMrtHK1QF8c8ScUTqE7ewIH93YzuotG6LhkfGQKrUqcm7CMNChNOEu83gQKMxAd2-yOqnyftZ2aJvwh5mOWAwIXQgL5w64xKqmg8Weo0YK8kVWkcP4QgvThvTzLKa-H1AiTHLmFYebuTj5oaKVcie7T7Pzx40THG1IK1aBbmKyjDXHk2w__&Key-Pair-Id=K3RDDB1TZ8BHT8')`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  height: 'auto',
+  width: 'auto', 
+  alignItems: 'initial',
+  justifyContent: 'center',
+  color: 'white',
+  textAlign: 'center',
+  fontSize: '25px',
+  fontFamily: ''
+};
+
 function FilmList() {
   const [films, setFilms] = useState([]);
   const user_id = sessionStorage.getItem("user_id");
@@ -27,7 +41,7 @@ function FilmList() {
   }, []);
 }
   return (
-    <div className="film-list">
+    <div style={backgroundStyle} className="film-list">
       {films.map((film) => (
         <div className="film-card" key={film.id}>
           <img src={film.image_url} alt={film.name} className="film-image" />
